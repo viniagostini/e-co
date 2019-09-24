6669d6e6462337b12b3c713fd95ea40c04d6eed4
Vinicius <viniciusaagostini@gmail.com>
2019-09-24 07:57:40 -0300

    add auth on user patch

--- server/user/user.route.js
+++ server/user/user.route.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const expressJwt = require('express-jwt')
+const config = require('../../config/config')
 
 const validate = require('express-validation')
 const paramValidation = require('./user.validator')
 
 const userCtrl = require('./user.controller')
+const authCtrl = require('../auth/auth.controller')
 
 const router = express.Router()
 
@@ -16,7 +19,7 @@ router.route('/:userDni')
   .get(userCtrl.getUserByDni)
 
   /**PATCH /api/user/:userDni - Update user by DNI */
-  .patch(userCtrl.updateUser)
+  .patch(expressJwt({ secret: config.jwtSecret }), authCtrl.checkUser, userCtrl.updateUser)
 
 
 /** Load user when API with userId route parameter is hit */
